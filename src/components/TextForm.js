4d4eee8fa3fc28eeb272bcc13f98821ca9fde1c4
RachatMad <madhupro10@gmail.com>
2024-08-06 17:47:13 +0530

    copied to clipboard

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -32,11 +32,11 @@ export default function TextForm(props) {
   }
 
   const handleCopy = () => {
-    let text = document.getElementById("myBox")
-    text.select();
-    document.getSelection().removeAllRanges();
-    navigator.clipboard.writeText(text.value);
-    props.showAlert("Text copied!","success")
+    // let text = document.getElementById("myBox")
+    // text.select();
+    // document.getSelection().removeAllRanges();
+    navigator.clipboard.writeText(text);
+    props.showAlert("Copied to clipboard!","success")
   }
 
   const handleExtraSpaces = () => {
